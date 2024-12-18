b888dac0d6dd8f6a1ea79ca4f228aadec36053ed
Kuba Klin <poczta@kubaklin.pl>
2024-12-18 20:28:29 +0100

    feat: bootstrap init

--- app/index.js
+++ app/index.js
@@ -1,6 +1,28 @@
 import './styles/styles.scss';
+import { Tooltip, Toast, Popover } from 'bootstrap'
 
-window.onload = () => {
-  const heading = document.querySelector('.heading');
-  heading.textContent = 'It\'s working!';
-};
+import { showUser } from './utilities/showUser';
+
+const usersWrapper = document.querySelector('.users-wrapper');
+
+Promise.all([
+  fetch('http://localhost:3000/users'),
+  fetch('http://localhost:3000/companies'),
+])
+  .then(function ([usersResponse, companiesResponse]) {
+    if (!usersResponse.ok) {
+      return Promise.reject({ status: usersResponse.status });
+    }
+    if (!companiesResponse.ok) {
+      return Promise.reject({ status: companiesResponse.status });
+    }
+    return Promise.all([usersResponse.json(), companiesResponse.json()]);
+  })
+  .then(function (responseBodies) {
+    const fetchedUsers = responseBodies[0];
+    const fetchedCompanies = responseBodies[1];
+    showUser(fetchedUsers, usersWrapper, fetchedCompanies);
+  })
+  .catch(function (error) {
+    console.log('Status code', error?.status);
+  });
