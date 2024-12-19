e27315d2a427ec0d3ed435cf3ed86b567cbfa739
Kuba Klin <poczta@kubaklin.pl>
2024-12-19 14:20:46 +0100

    fix: variables rename

--- app/utilities/sortCompanies.js
+++ app/utilities/sortCompanies.js
@@ -1,8 +1,8 @@
 export function sortCompanies(accordionWrapper) {
   window.addEventListener('load', function () {
     let elements = Array.from(accordionWrapper.children);
-    let sorted = elements.sort(function (a, b) {
-      return +a.dataset.employees - +b.dataset.employees;
+    let sorted = elements.sort(function (firstElement, secondElement) {
+      return +firstElement.dataset.employees - +secondElement.dataset.employees;
     });
     accordionWrapper.innerHTML = '';
     sorted.forEach(function (element) {
