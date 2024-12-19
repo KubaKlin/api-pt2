78b5d9ccc37832ffabe5fbaea1156533269267b5
Kuba Klin <poczta@kubaklin.pl>
2024-12-19 08:29:52 +0100

    feat: count company employees function and styles

--- app/utilities/filterCompanyEmployees.js
+++ app/utilities/filterCompanyEmployees.js
@@ -1,10 +1,17 @@
-export function filterCompanyEmployees(company, fetchedUsers, accordionBody) {
+export function filterCompanyEmployees(company, fetchedUsers, accordionBody, accordionButton) {
+  let employeeCount = 0;
+  const employeeCountIndicator = document.createElement('div');
+  employeeCountIndicator.classList.add('employee-count');
+  accordionButton.appendChild(employeeCountIndicator);
   return fetchedUsers.filter(function (employee) {
 
     if (employee.uris.company === company.uri) {
+      employeeCount++;
       const employeeWrapper = document.createElement('div');
       employeeWrapper.innerText = `name: ${employee.name} & mail: ${employee.email}`;
       accordionBody.appendChild(employeeWrapper);
+      employeeCountIndicator.innerText = `employees in the company: ${employeeCount}`;
     }
   });
+
 }
--- app/utilities/showUser.js
+++ app/utilities/showUser.js
@@ -1,11 +1,35 @@
 import { filterCompanyEmployees } from './filterCompanyEmployees';
-import {createStructure} from './createStructure';
 
 export function showUser(accordionWrapper, fetchedCompanies, fetchedUsers) {
   fetchedCompanies.forEach(function (company, index) {
     const accordionBody = document.createElement('div');
     accordionBody.classList.add('accordion-body');
-    createStructure(company, index, accordionBody, accordionWrapper);
-    filterCompanyEmployees(company, fetchedUsers, accordionBody);
+    const accordionItem = document.createElement('div');
+    accordionItem.classList.add('accordion-item');
+
+    const companyHeader = document.createElement('h2');
+    companyHeader.classList.add('accordion-header');
+    companyHeader.setAttribute('id', 'heading' + index);
+
+    const accordionButton = document.createElement('button');
+    accordionButton.classList.add('accordion-button');
+    accordionButton.classList.add('collapsed');
+    accordionButton.setAttribute('data-bs-toggle', 'collapse');
+    accordionButton.setAttribute('data-bs-target', '#collapse' + index);
+    accordionButton.innerText = company.name;
+
+    const accordion = document.createElement('div');
+    accordion.classList.add('accordion-collapse');
+    accordion.classList.add('collapse');
+    accordion.setAttribute('id', 'collapse' + index);
+    accordion.setAttribute('aria-labelleddby', 'heading' + index);
+
+    accordionWrapper.appendChild(accordionItem);
+    accordionItem.appendChild(companyHeader);
+    accordionItem.appendChild(accordion);
+    accordion.appendChild(accordionBody);
+    companyHeader.appendChild(accordionButton);
+
+    filterCompanyEmployees(company, fetchedUsers, accordionBody, accordionButton);
   });
 }
