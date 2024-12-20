c7e66114c6ca622b6df311a2788e444126fc1517
Kuba Klin <poczta@kubaklin.pl>
2024-12-20 09:34:54 +0100

    fix: window load remove

--- app/utilities/sortCompanies.js
+++ app/utilities/sortCompanies.js
@@ -1,12 +1,10 @@
 export function sortCompanies(accordionWrapper) {
-  window.addEventListener('load', function () {
-    const elements = Array.from(accordionWrapper.children);
-    const sorted = elements.sort(function (firstElement, secondElement) {
-      return +firstElement.dataset.employees - +secondElement.dataset.employees;
-    });
-    accordionWrapper.innerHTML = '';
-    sorted.forEach(function (element) {
-      accordionWrapper.append(element);
-    });
+  const elements = Array.from(accordionWrapper.children);
+  const sorted = elements.sort(function (firstElement, secondElement) {
+    return +firstElement.dataset.employees - +secondElement.dataset.employees;
+  });
+  accordionWrapper.innerHTML = '';
+  sorted.forEach(function (element) {
+    accordionWrapper.append(element);
   });
 }
