51861c342d75d28215d88be72146eaf5e271d969
Kuba Klin <poczta@kubaklin.pl>
2024-12-29 21:14:14 +0100

    fix: prettier run

--- app/utilities/filterCompanyEmployees.js
+++ app/utilities/filterCompanyEmployees.js
@@ -1,16 +1,16 @@
 export function filterCompanyEmployees(fetchedCompanies, fetchedUsers) {
   const companiesDictionary = {};
-  fetchedCompanies.forEach(function(company, index) {
+  fetchedCompanies.forEach(function (company, index) {
     companiesDictionary[index] = Object.assign({}, company, { employees: [] });
   });
 
   const companiesByUri = {};
-  Object.keys(companiesDictionary).forEach(function(index) {
+  Object.keys(companiesDictionary).forEach(function (index) {
     const company = companiesDictionary[index];
     companiesByUri[company.uri] = company;
   });
 
-  fetchedUsers.forEach(function(user) {
+  fetchedUsers.forEach(function (user) {
     const companyUri = user.uris.company;
     if (companiesByUri[companyUri]) {
       companiesByUri[companyUri].employees.push(user);
